35ae9252de92cf8f4430d152057bb6baf6290c98
sithish <sithishkumar2003@gmail.com>
2025-02-07 10:20:19 -0800

    day1.py file added

---
+++ day1.py
@@ -0,0 +1,6 @@
+print("welcome to the band name generator")
+a=input("what's is the name of the city grew up in ?")
+
+b=input("what's your pet's name?")
+
+print("your band name could be " + a + " " + b ) 
